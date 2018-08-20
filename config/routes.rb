0c82f9a25b80102144ebe450332cf1d6140c27a1
Oscar Luza <onluza@gmail.com>
2018-08-20 03:54:34 -0500

    controllers moved to api/v1 module

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  post 'auth/login', to: 'authentication#authenticate'
-  post 'signup', to: 'users#create'
 
+  namespace :api do
+    namespace :v1 do
+      post 'users/login', to: 'authentication#authenticate'
+      post 'users/signup', to: 'users#create'
+    end
+  end
 end
